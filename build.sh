48ecd92c9ee94cd14605b607ddf65e4940c8513a
Jan Hrček <honza.hrk@gmail.com>
2022-07-31 16:29:50 +0200

    Bump elm/virtual-dom, make build.sh work with latest uglifyjs

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ OUT=dist/js/app.js
 elm make src/Main.elm --output=$OUT --optimize
 
 uglifyjs $OUT --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-| uglifyjs --mangle --output=$OUT
+| uglifyjs --mangle --output $OUT
